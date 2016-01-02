af69e6ab757d10835d2a53d0fa4b8b1bc5e3aeeb
Connor Adsit <connor.adsit@gmail.com>
2016-01-01 20:17:06 -0500

    Compiles in Visual studio!

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	cout << "Hello World" << endl;
+	cout << "Enter to exit..." << endl;
+	string crap;
+	getline(cin, crap);
+	return 0;
+}
