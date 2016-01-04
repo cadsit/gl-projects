918f6627712f42a799d8f974b359cf35a57abd51
Connor Adsit <connor.adsit@gmail.com>
2016-01-04 16:43:41 -0500

    I made a window!

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <string>
+#include "GL\glew.h"
+#include "GL\freeglut.h"
 
 using namespace std;
 
+void display() {};
+
 int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE);
+	glutInitWindowSize(300, 300);
+	glutInitWindowPosition(500, 500);
+	glutCreateWindow("Hello World");
+	glutDisplayFunc(display);
+	glutMainLoop();
 	cout << "Hello World" << endl;
 	cout << "Enter to exit..." << endl;
 	string crap;
